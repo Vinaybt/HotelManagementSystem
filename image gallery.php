328c47deeb6ef61b7c99a4cecb385a60d62d8483
vinay B T <vinaybtnofearv@gmail.com>
2021-08-31 21:53:24 +0530

    Initial commit

---
+++ image gallery.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><!--Head Open  Here-->
+  <title>Rosewood Hotel</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="css/style.css"rel="stylesheet"/>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head> <!--Head Open  Here-->
+<body style="margin-top:50px;"> 
+  <?php
+      include('Menu Bar.php')
+  ?>
+<div class="container">
+  <h2> Gallery</h2>
+  <hr/>
+  <h2> Bar </h2>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/bar.jpg" target="_blank">
+          <img src="image/image%20gallery/bar.jpg" alt="img1 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/bar1.jpg" target="_blank">
+          <img src="image/image%20gallery/bar1.jpg" alt="img2 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/bar2.jpg" target="_blank">
+          <img src="image/image%20gallery/bar2.jpg" alt="img3 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    
+    <h2> Rooms</h2>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/Delux Room Single Bed Ac Room/room.jpg" target="_blank">
+          <img src="image/image%20gallery/room.jpg" alt="img3 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/Delux Room Single Bed Ac Room/img3.jpg" target="_blank">
+          <img src="image/image%20gallery/room1.jpg" alt="img3 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/Delux Room Single Bed Ac Room/img4.jpg" target="_blank">
+          <img src="image/image%20gallery/room2.jpg" alt="img3 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <h2> Swimming Pool</h2>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/swim.jpg" target="_blank">
+          <img src="image/image%20gallery/swim.jpg" alt="img4 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/swim1.jpg" target="_blank">
+          <img src="image/image%20gallery/swim1.jpg" alt="img5 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/swim2.jpg" target="_blank">
+          <img src="image/image%20gallery/swim2.jpg" alt="img6 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+    <h2> Gym </h2>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/gym1.jpg" target="_blank">
+          <img src="image/image%20gallery/gym1.jpg" alt="img7 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+     <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%2520gallery/gym3.jpg" target="_blank">
+          <img src="image/image%20gallery/gym3.jpg" alt="img7 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+     <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/gym.jpg" target="_blank">
+          <img src="image/image%20gallery/gym.jpg" alt="img7 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+       <h2> Parking Area </h2>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/park.jpg" target="_blank">
+          <img src="image/image%20gallery/park.jpg" alt="img7 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+     <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="image/image%20gallery/park1.jpg" target="_blank">
+          <img src="image/image%20gallery/park1.jpg" alt="img7 Not Show" style="width:100%;height:160px;">
+        </a>
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+
